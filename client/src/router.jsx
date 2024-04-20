9272885bae2bd719ba3ca46d4306aa616c750ef8
Mickey <mickeyth59@gmail.com>
2024-04-20 19:33:01 +0530

    setup for login signout in client

---
+++ client/src/router.jsx
@@ -0,0 +1,44 @@
+import {
+  Navigate,
+  Route,
+  createBrowserRouter,
+  createRoutesFromElements,
+} from "react-router-dom";
+import Register from "./pages/Authentication/Register";
+import Profile from "./pages/Authentication/Profile";
+import Recovery from "./pages/Authentication/Recovery";
+import Reset from "./pages/Authentication/Reset";
+import Password from "./pages/Authentication/Password";
+import Username from "./pages/Authentication/Username";
+
+
+
+
+import { AuthorizeUser, ProtectRoute } from './middleware/auth'
+import Admin from "./pages/AdminDash/Admin";
+
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <>
+      {/* <Route path="/" element={<Layout />}> */}
+
+
+
+        <Route path="/" element={<Register />} />
+        <Route path="adminpage" element={<Admin />} />
+        <Route path="profile" element={<ProtectRoute><Profile /></ProtectRoute>} />
+        <Route path="recovery" element={<Recovery />} />
+        <Route path="reset" element={<Reset />} />
+        <Route path="password" element={<Password />} />
+        <Route path="username" element={<Username />} />
+       
+     
+      {/* </Route> */}
+    </>
+  ),
+  // { basename: import.meta.env.DEV ? "/" : "/react-face-auth/" }
+  { basename: "/" }
+);
+
+export default router;
---
+++ client/vite.config.js
@@ -0,0 +1,12 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+// https://vitejs.dev/config/
+export default defineConfig(({ command }) => {
+  const config = {
+    plugins: [react()],
+    base: "/",
+  }; 
+
+  return config; // Add this line to properly return the configuration object
+});
