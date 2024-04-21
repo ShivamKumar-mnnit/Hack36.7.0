055ef40a32e36e335dd998f9926c462c388bfb39
anupamxy <123785384+anupamxy@users.noreply.github.com>
2024-04-21 08:19:22 +0530

    final update

--- client/src/pages/Dashboard/Dashboard.jsx
+++ client/src/pages/Dashboard/Dashboard.jsx
@@ -48,14 +48,14 @@ const Dashboard = () => {
       <div class="flex-1 p-4">
         <div id="car-box" class="bg-blue-500 text-white p-6 rounded-lg shadow-lg transform hover:scale-105 transition duration-300">
          
-          <h2 class="text-2xl font-bold mb-4">option1</h2>
+          <h2 class="text-2xl font-bold mb-4">Call to Doctor</h2>
           {/* <p>This is a car box.</p> */}
         </div>
       </div>
       <div class="flex-1 p-4">
         <div id="truck-box" class="bg-blue-500 text-white p-6 rounded-lg shadow-lg transform hover:scale-105 transition duration-300">
           
-          <h2 class="text-2xl font-bold mb-4">option1</h2>
+          <h2 class="text-2xl font-bold mb-4">Call to Ambulance</h2>
           {/* <p>This is a truck box.</p> */}
         </div>
       </div>
@@ -64,16 +64,17 @@ const Dashboard = () => {
       <div class="flex-1 p-4">
         <div id="suv-box" class="bg-green-500 text-white p-6 rounded-lg shadow-lg transform hover:scale-105 transition duration-300">
           
-          <h2 class="text-2xl font-bold mb-4">option1</h2>
+          <h2 class="text-2xl font-bold mb-4">Call to Police</h2>
           {/* <p>This is an SUV box.</p> */}
         </div>
       </div>
       <div class="flex-1 p-4">
         <div id="bike-box" class="bg-yellow-500 text-white p-6 rounded-lg shadow-lg transform hover:scale-105 transition duration-300">
           
-          <h2 class="text-2xl font-bold mb-4">option1</h2>
+          <h2 class="text-2xl font-bold mb-4">Call to Family</h2>
           {/* <p>This is a bike box.</p> */}
         </div>
+        
       </div>
     </div>
   </div>
